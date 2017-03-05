d9655098cdee8e601bb1c41efb73291629d815e7
Jonathan Cherryhomes <Jonathan Cherryhomes>
2017-03-04 22:00:21 -0600

    updated break_time.py created rename files lesson

---
+++ RenameFiles/rename_files.py
@@ -0,0 +1,17 @@
+import os
+
+
+def rename_files():
+    current_directory = os.path.dirname(__file__)
+    working_directory = os.path.join(current_directory, "prank")
+    
+    files = os.listdir(working_directory)
+
+    os.chdir(working_directory)
+
+    for file_name in files:
+        os.rename(file_name, file_name.translate(None, "0123456789"))    
+
+    os.chdir(current_directory)
+
+rename_files()
--- TakeABreak/break_time.py
+++ TakeABreak/break_time.py
@@ -3,12 +3,14 @@ import datetime as DT
 import webbrowser
 
 # Everything is in seconds for consistency
-sleep_time = 600 # Duration in seconds - 10 minutes
-total_duration = 1800 # How long to run the program - 30 minutes
-current_time = DT.datetime.now() # Current time
-end_time = current_time + DT.timedelta(seconds=total_duration) #Current time offset by total duration
+sleep_time = 3600 # Duration in seconds before a break
+number_of_breaks = 3 # The number of breaks that will be taken
+total_duration = sleep_time * number_of_breaks # Total time the program will run
 
-print("this program started at " + current_time.strftime("%H:%S, %d %B %Y"))
+current_time = DT.datetime.now() # Current time
+end_time = current_time + DT.timedelta(seconds=total_duration) # Current time offset by total duration
+
+print("this program started at " + time.strftime("%H:%S, %d %B %Y"))
 
 while(current_time <= end_time):
     time.sleep(sleep_time)
