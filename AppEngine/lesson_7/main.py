21a250f0ed810b3474e77edfe77b9a6aa2797903
Jonathan Cherryhomes <Jonathan Cherryhomes>
2017-07-19 01:03:54 -0500

    updated app.yaml to remove fields that are not needed for appengine

--- AppEngine/lesson_7/main.py
+++ AppEngine/lesson_7/main.py
@@ -1,60 +1,131 @@
 import os
+import random
+import re
 import webapp2
+import jinja2
 import hashlib
 import hmac
+import string
+
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 SECRET = 'imsosecret'
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+PASSWORD_RE = re.compile(r"^.{3,20}$")
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+
+def valid_username(username):
+	return USER_RE.match(username)
+
+def valid_password(password):
+	return PASSWORD_RE.match(password)
+
+def valid_verify(password, verify):
+	return valid_password(password) and password == verify
+
+def valid_email(email):
+	return not email or EMAIL_RE.match(email)
 
 def make_salt():
-    return ''.join(random.choice(string.letters) for x in xrange(5)
+    return ''.join(random.choice(string.letters) for x in xrange(5))
+
+def valid_pw(name, pw, h):
+	try:
+		salt = h.split('|')[1]
+		hash = hashlib.sha256(name + pw + salt).hexdigest()
+		return h == ('%s|%s' % (hash, salt))
+	except: 
+		return None
 
 def make_pw_hash(name, pw):
 	salt = make_salt()
 	h = hashlib.sha256(name + pw + salt).hexdigest()
 	return '%s|%s' % (h, salt)
 
-def hash_str(s):
-    return hmac.new(SECRET, s).hexdigest()
+class User(db.Model):
+	username = db.StringProperty(required = True)
+	password = db.StringProperty(required = True)
+	created = db.DateTimeProperty(auto_now_add = True)
 
-def make_secure_val(s):
-    return "%s|%s" % (s, hash_str(s))
+class Handler(webapp2.RequestHandler):
+	def write(self, *a, **kw):
+		self.response.out.write(*a, **kw)
 
-# -----------------
-# User Instructions
-# 
-# Implement the function check_secure_val, which takes a string of the format 
-# s,HASH
-# and returns s if hash_str(s) == HASH, otherwise None 
+	def render_str(self, template, **params):
+		t = jinja_env.get_template(template)
+		return t.render(params)
 
-def check_secure_val(response, h):
-	strings = h.split("|")
+	def render(self, template, **kw):
+		self.write(self.render_str(template, **kw))
 
-	if not len(strings) == 2:
-		return None
-
-	val = strings[0]
-	hash = strings[1]
-	if hash == hash_str(val):
-		return val
-
-class MainPage(webapp2.RequestHandler):
+class MainPage(Handler):
 	def get(self):
-		visit_cookie = self.request.cookies.get('visits')
-		visits = 0
+		user_cookie = self.request.cookies.get('user')
 
-		if visit_cookie:
-			cookie_val = check_secure_val(self.response, visit_cookie)
-
-			if cookie_val:
-				visits = int(cookie_val)
-
-		visits += 1
-
-		self.response.headers.add_header('Set-Cookie', 'visits=%s' % make_secure_val(str(visits)))
-
-		if visits > 10000:
-			self.response.out.write("You are the best ever!")
+		if not user_cookie:
+			self.redirect("/signup")
 		else:
-			self.response.out.write("You have visited %s times" % visits)
+			strings = user_cookie.split('|')
+			id = int(strings[0])
+			hash = '%s|%s' % (strings[1], strings[2])
 
-app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
+			user = User.get_by_id(id)
+			
+			if not user:
+				self.redirect("/signup")
+			elif not valid_pw(user.username, user.password, hash):
+				self.redirect("/signup")
+			else:
+				self.render("main.html", username=user.username)	
+
+class SignupPage(Handler):
+	def get(self):
+		self.render("signup.html")
+
+	def post(self):
+		username_error = ""
+		password_error = ""
+		verify_error = ""
+		email_error = ""
+		
+		username = self.request.get("username")
+		email = self.request.get("email")
+		password = self.request.get("password")
+		verify = self.request.get("verify")
+
+		if not valid_username(username):
+			username_error = "Username is invalid"
+
+		if not valid_password(password):
+			password_error = "Password is not valid"
+
+		if not valid_email(email):
+			email_error = "Email is not valid"
+
+		if not valid_verify(password, verify):
+			verify_error = "Your passwords didn't match"
+
+		if valid_email(email) and valid_username(username) and valid_verify(password, verify):
+			user = User(username=username, password=password)
+			user.put()
+
+			id = user.key().id()
+			hash = make_pw_hash(username, password)
+			self.response.headers.add_header('Set-Cookie', 'user=%s|%s; Path=/' % (str(id), hash))
+			self.redirect("/")
+			
+		else:
+			self.render(
+				"signup.html", 
+				username = username, 
+				username_error = username_error, 
+				password_error = password_error, 
+				email_error = email_error,
+				verify_error = verify_error)
+
+app = webapp2.WSGIApplication([
+	('/', MainPage),
+	('/signup', SignupPage)], debug=True)
